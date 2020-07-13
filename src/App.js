e0d1099b649a68db5097d688a0b01d84cefcdafb
agraniero <4wowbagger2@gmail.com>
2020-07-13 16:37:02 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={this.usernameSubmitHandler} className="username-container">
+      <h1>React Instant Chat</h1>
+      <div>
+        <input
+          type="text"
+          onChange={this.usernameChangeHandler}
+          placeholder="Enter a username..."
+          required />
+      </div>
+      <input type="submit" value="Submit" />
+    </form>
   );
 }
 
